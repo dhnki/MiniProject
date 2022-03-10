c9f6b682a5968cd2dc13d7f98fc3119d026fafd1
dhnkim <0deahwan0@naver.com>
2022-03-10 14:38:53 +0900

    Update .eslintrc.js

--- board-app/.eslintrc.js
+++ board-app/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
       {
         singleQuote: true,
         semi: false,
-        useTabs: true,
+        useTabs: false,
         tabWidth: 2,
         trailingComma: "all",
         printWidth: 80,
